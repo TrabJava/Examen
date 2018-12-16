121b3b38809fbc2a02d49c489b581d0efc9853ec
TrabJava <42870972+TrabJava@users.noreply.github.com>
2018-12-16 15:01:34 -0300

    Metodo  Modificar_docentes

--- src/java/modelo/dao/JuegoFacade.java
+++ src/java/modelo/dao/JuegoFacade.java
@@ -5,11 +5,9 @@
  */
 package modelo.dao;
 
-import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import modelo.dto.Juego;
 
 /**
@@ -30,10 +28,5 @@ public class JuegoFacade extends AbstractFacade<Juego> {
     public JuegoFacade() {
         super(Juego.class);
     }
- 
-    
-    public List<Juego>ListarJuegoosPS4(){
-        Query query= em.createNamedQuery("Juego.PORPS4");
-        return query.getResultList();
-    }
+
 }
