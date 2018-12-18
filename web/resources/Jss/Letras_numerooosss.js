33c073dc253240af86ba902efd71b0ed187ff983
TrabJava <42870972+TrabJava@users.noreply.github.com>
2018-12-18 00:22:44 -0300

    Validé todas las weas de paginas

---
+++ web/resources/Jss/Letras_numerooosss.js
@@ -0,0 +1,18 @@
+function Letras_numerossss(e) {
+    key = e.keyCode || e.which;
+    tecla = String.fromCharCode(key).toLowerCase();
+    letras = "qwertyuiopasdfghjklñzxcvbnm_1234567890";
+    especiales = "8-37-39-46";
+    tecla_especial = false
+    for (var i in especiales) {
+        if (key == especiales[i]) {
+            tecla_especial = true;
+            break;
+        }
+    }
+    if (letras.indexOf(tecla) == -1 && !tecla_especial) {
+        return false;
+    }
+}
+
+
